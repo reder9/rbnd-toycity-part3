2136d657dc0184f5e660dea5feb15fa7f1ce46ee
Caleb Reder <caleb.reder@code42.com>
2016-06-28 22:19:35 -0500

    Product section is working

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,39 @@
-# product.rb
-
 class Product
+  attr_reader :title, :stock, :price
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    @stock = options[:stock]
+    @price = options[:price]
+    add_to_products
   end
 
   def self.all
     @@products
   end
+
+  def in_stock?
+      stock > 0
+  end
+
+  private
+
+  def add_to_products
+    if @@products.include?  self.title
+      raise DuplicateProductError, "Error, " + self.title + "already exists"
+    else
+      @@products << self
+    end
+  end
+
+  def self.find_by_title(title)
+  @@products.find { |product| product.title == title }
+  end
+
+  def self.in_stock
+    @@products.select { |product| product.in_stock?}
+  end
+
 end
